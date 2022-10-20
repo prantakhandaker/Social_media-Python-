087dc0e12a03c4e848d119b652b0a4ac9914fd3c
prantakhandaker <prantakhandaker29@gmail.com>
2022-10-20 15:03:59 +0600

    Post Upload

--- core/models.py
+++ core/models.py
@@ -18,12 +18,12 @@ class profile(models.Model):
 
 
 class post(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
-    user = models.CharField(max_length = 100)
-    image = models.ImageField(upload_to = 'post_images')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
-    created_at = models.DateTimeField(default = datetime.now)
-    no_of_likes = models.IntegerField(default = 0)
-
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.user
--- core/views.py
+++ core/views.py
@@ -3,20 +3,32 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import profile
+from .models import profile, post
 
 # Create your views here.
 
 @login_required(login_url= 'signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
-    user_profile = profile.objects.get(user=user_object)
-    return render(request, 'index.html',{'user_profile':user_profile})
+    # user_profile = profile.objects.get(user=user_object)
+    return render(request, 'index.html')
 
 
 @login_required(login_url= 'signin')
 def upload(request):
-    return 
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+
+        new_post = post.objects.create(user = user , image = image , caption = caption)
+        new_post.save()
+        return redirect ('/')
+
+
+    else:
+        return redirect ('/')
 
 
 def signup(request):
