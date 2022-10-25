d493a88001f46fe93d9b97833099695d7a1d7ab1
prantakhandaker <prantakhandaker29@gmail.com>
2022-10-25 14:59:37 +0600

    like

--- core/models.py
+++ core/models.py
@@ -7,23 +7,31 @@ user = get_user_model()
 
 # Create your models here.
 class profile(models.Model):
-    user = models.ForeignKey(user, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
-    bio = models.TextField(blank=True)
+    user        = models.ForeignKey(user, on_delete=models.CASCADE)
+    id_user     = models.IntegerField()
+    bio         = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location = models.CharField(max_length=100, blank=True)
+    location    = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
 
 
 class post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images')
-    caption = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now)
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user        = models.CharField(max_length=100)
+    image       = models.ImageField(upload_to='post_images')
+    caption     = models.TextField()
+    created_at  = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
     
     def __str__(self):
         return self.user
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
--- core/views.py
+++ core/views.py
@@ -3,24 +3,25 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import profile, post
+from .models import profile, post, LikePost
 
 # Create your views here.
 
 @login_required(login_url= 'signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
-    user_profile = profile.objects.get(user=user_object)
+    # user_profile = profile.objects.get(user=user_object)
 
     posts = post.objects.all()
-    return render(request, 'index.html', {'user_profile':user_profile,'posts':posts})
+    return render(request, 'index.html', {'posts':posts})
 
 @login_required(login_url= 'signin')
 def upload(request):
-    if request.method == 'POST':
-        user = request.user.username
-        image = request.FILES.get('image_upload')
-        caption = request.POST['caption']
+
+    if request.method   == 'POST':
+        user            = request.user.username
+        image           = request.FILES.get('image_upload')
+        caption         = request.POST['caption']
 
 
         new_post = post.objects.create(user = user , image = image , caption = caption)
@@ -32,14 +33,40 @@ def upload(request):
         return redirect ('/')
 
 
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id  = request.GET.get('post_id')
+
+    
+
+    post = post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like   = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes +1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes -1
+        post.save()
+        return redirect('/')
+
+
+
 def signup(request):
 
     if request.method == 'POST':
 
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
+        username    = request.POST['username']
+        email       = request.POST['email']
+        password    = request.POST['password']
+        password2   = request.POST['password2']
         
         if password == password2:
             if User.objects.filter(email=email).exists():
@@ -61,7 +88,7 @@ def signup(request):
 
 
                 # create a profile object for new user
-                user_model = User.objects.get(username=username)
+                user_model  = User.objects.get(username=username)
                 new_profile = profile.objects.create(user = user_model, id_user = user_model.id)
                 new_profile.save()
                 return redirect('settings')
@@ -101,16 +128,16 @@ def logout(request):
 def settings(request):
     user_profile = profile.objects.get(user=request.user)
 
-    if request.method == 'POST':
+    if request.method   == 'POST':
         if request.FILES.get('image') == None:
-            image = user_profile.profile_img
-            bio = request.POST['bio']
-            location = request.POST['location']
+            image       = user_profile.profile_img
+            bio         = request.POST['bio']
+            location    = request.POST['location']
 
 
-            user_profile.profile_img = image
-            user_profile.bio = bio
-            user_profile.location = location
+            user_profile.profile_img    = image
+            user_profile.bio            = bio
+            user_profile.location       = location
             user_profile.save()
 
         if request.FILES.get('image') != None:
@@ -119,9 +146,9 @@ def settings(request):
             location = request.POST['location']
 
 
-            user_profile.profile_img = image
-            user_profile.bio = bio
-            user_profile.location = location
+            user_profile.profile_img    = image
+            user_profile.bio            = bio
+            user_profile.location       = location
             user_profile.save()
 
         return redirect('settings')
